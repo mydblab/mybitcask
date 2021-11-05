4836e2e9605945cc027935693454dc8d40864902
0x5457 <0x5457@protonmail.com>
2021-11-05 13:42:28 +0800

    fix bug

--- src/io_test.cc
+++ src/io_test.cc
@@ -13,12 +13,14 @@ TEST(IoTest, FStreamSequentialWriter) {
   auto tempfile = test::MakeTempFile("mybitcask_test_", ".tmp");
   ASSERT_TRUE(tempfile.ok());
 
+  std::cout << tempfile->Filename() << std::endl;
+
   auto writer = OpenSequentialWriter(tempfile->Filename());
   ASSERT_TRUE(writer.ok());
   std::string_view testdata = "test data.";
   auto append_status =
       (*writer)->Append({reinterpret_cast<const std::uint8_t*>(testdata.data()),
-                         sizeof testdata});
+                         testdata.size()});
   ASSERT_TRUE(append_status.ok());
   auto sync_status = (*writer)->Sync();
   ASSERT_TRUE(sync_status.ok());
