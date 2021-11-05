f235f0870528ed1fcb38576b1180d3b6d4495b6a
0x5457 <0x5457@protonmail.com>
2021-11-05 10:03:11 +0800

    io windows

---
+++ src/io_windows.cc
@@ -0,0 +1,34 @@
+#include "io.h"
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <cstring>
+
+namespace mybitcask {
+namespace io {
+
+class WindowsRandomAccessReader : public RandomAccessReader {
+ public:
+  WindowsRandomAccessReader() = delete;
+  ~WindowsRandomAccessReader() override {}
+
+  absl::StatusOr<std::size_t> ReadAt(
+      uint64_t offset, absl::Span<std::uint8_t> dst) const noexcept override {
+    return 0;
+  }
+
+ private:
+  WindowsRandomAccessReader() {}
+
+  friend absl::StatusOr<std::unique_ptr<RandomAccessReader>>
+  OpenRandomAccessReader(ghc::filesystem::path filename);
+};
+
+absl::StatusOr<std::unique_ptr<RandomAccessReader>> OpenRandomAccessReader(
+    ghc::filesystem::path filename) {
+  return nullptr;
+}
+
+}  // namespace io
+}  // namespace mybitcask
