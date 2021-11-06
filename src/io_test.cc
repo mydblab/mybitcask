088b480236da605ed0932c3253bb2928fc467f9d
0x5457 <0x5457@protonmail.com>
2021-11-06 08:00:21 +0800

    fixed bug

--- src/io_test.cc
+++ src/io_test.cc
@@ -11,21 +11,26 @@ namespace io {
 TEST(IoTest, SequentialWriter) {
   auto tempfile = test::MakeTempFile("mybitcask_test_", ".tmp");
   ASSERT_TRUE(tempfile.ok());
-  auto writer = OpenSequentialWriter(tempfile->Filename());
-  ASSERT_TRUE(writer.ok());
+
   const std::string test_data = "test data.";
 
-  auto append_status = (*writer)->Append(
-      {reinterpret_cast<const std::uint8_t*>(test_data.c_str()),
-       test_data.size()});
-  ASSERT_TRUE(append_status.ok());
-  auto sync_status = (*writer)->Sync();
-  ASSERT_TRUE(sync_status.ok());
-  (*writer).release();
+  {
+    auto writer = OpenSequentialWriter(tempfile->Filename());
+    ASSERT_TRUE(writer.ok());
+    auto append_status = (*writer)->Append(
+        {reinterpret_cast<const std::uint8_t*>(test_data.c_str()),
+         test_data.size()});
+    ASSERT_TRUE(append_status.ok());
+    auto sync_status = (*writer)->Sync();
+    ASSERT_TRUE(sync_status.ok());
+    // The writer outside the current scope will be destructed
+  }
 
   std::ifstream tempfile_stream(tempfile->Filename());
   std::stringstream read_data;
   read_data << tempfile_stream.rdbuf();
+  tempfile_stream.close();
+
   EXPECT_EQ(read_data.str(), test_data);
 }
 
