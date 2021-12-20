450e653bfa02144ea9c94951c71b7000186863a6
0x5459 <0x5459@protonmail.com>
2021-12-20 19:18:18 +0800

    update

--- src/store_hint.cc
+++ src/store_hint.cc
@@ -14,7 +14,36 @@ const std::uint16_t kTombstone = 0xFFFF;
 
 const std::uint32_t kKeyLenLen = 1;
 const std::uint32_t kValLenLen = 2;
-const std::uint32_t kHeaderLen = kKeyLenLen + kValLenLen;
+const std::uint32_t kValPosLen = 4;
+const std::uint32_t kHeaderLen = kKeyLenLen + kValLenLen + kValPosLen;
+
+class RawHeader final {
+ public:
+  RawHeader(std::uint8_t* const data) : data_(data) {}
+
+  std::uint8_t key_len() const { return data_[0]; }
+  std::uint16_t value_len() const {
+    return is_tombstone() ? 0 : raw_value_len();
+  }
+  std::uint32_t value_pos() const {
+    return absl::little_endian::Load32(&data_[kKeyLenLen + kValLenLen]);
+  }
+  bool is_tombstone() const { return raw_value_len() == kTombstone; }
+  void set_key_len(std::uint8_t key_len) { data_[0] = key_len; }
+  void set_value_len(std::uint16_t value_len) {
+    absl::little_endian::Store16(&data_[kKeyLenLen], value_len);
+  }
+  void set_value_pos(std::uint32_t value_pos) const {
+    absl::little_endian::Store32(&data_[kKeyLenLen + kValLenLen], value_pos);
+  }
+  void set_tombstone() { set_value_len(kTombstone); }
+
+ private:
+  std::uint16_t raw_value_len() const {
+    return absl::little_endian::Load16(&data_[kKeyLenLen]);
+  }
+  std::uint8_t* const data_;
+};
 
 Generator::Generator(log::Reader* log_reader, const ghc::filesystem::path& path)
     : log_reader_(log_reader), path_(path) {}
@@ -40,35 +69,24 @@ absl::Status Generator::Generate(std::uint32_t file_id) noexcept {
   }
 
   log_reader_->key_iter(file_id, file_id)
-      .Fold<Void>(Void(), [&](Void&&, log::Key&& key) {
-        log_file.write();
-        return Void();
-      });
+      .Fold<Void>(Void(), [&](Void&&, log::Key&& key) { return Void(); });
 
+  /*
+    std::uint8_t header_data[kHeaderLen]{};
+        RawHeader header(header_data);
+        // write header
+        hint_file.write(reinterpret_cast<char*>(header_data), kHeaderLen);
+
+        if (hint_file.eof()) {
+          return acc;
+        }
+        if (hint_file.fail()) {
+          return absl::InternalError(kErrRead);
+        }
+
+  */
   return absl::Status();
 }
-
-class RawHeader final {
- public:
-  RawHeader(std::uint8_t* const data) : data_(data) {}
-
-  std::uint8_t key_len() const { return data_[0]; }
-  std::uint16_t value_len() const {
-    return is_tombstone() ? 0 : raw_value_len();
-  }
-  bool is_tombstone() const { return raw_value_len() == kTombstone; }
-  void set_key_len(std::uint8_t key_len) { data_[0] = key_len; }
-  void set_value_len(std::uint16_t value_len) {
-    absl::little_endian::Store16(&data_[kKeyLenLen], value_len);
-  }
-  void set_tombstone() { set_value_len(kTombstone); }
-
- private:
-  std::uint16_t raw_value_len() const {
-    return absl::little_endian::Load16(&data_[kKeyLenLen]);
-  }
-  std::uint8_t* const data_;
-};
 
 template <typename T>
 absl::StatusOr<T> FoldKeys(absl::string_view hint_filepath, T init,
@@ -88,9 +106,13 @@ absl::StatusOr<T> FoldKeys(absl::string_view hint_filepath, T init,
     if (file.fail()) {
       return absl::InternalError(kErrRead);
     }
-    RawHeader(header_data);
-    entry.key.resize(entry.key_sz);
-    file.read(reinterpret_cast<char*>(entry.key.data()), entry.key_sz);
+    RawHeader header(header_data);
+    Entry entry{};
+    entry.value_pos = header.is_tombstone() ? absl::nullopt
+                                            : EntryValuePos{header.value_len(),
+                                                            header.value_pos()};
+    entry.key.resize(header.key_len());
+    file.read(reinterpret_cast<char*>(entry.key.data()), header.key_len());
     if (file.fail()) {
       return absl::InternalError(kErrRead);
     }
---
+++ src/store_hint_test.cc
@@ -0,0 +1,9 @@
+#include "store_hint.h"
+
+namespace mybitcask {
+namespace store {
+namespace hint {
+
+}
+}  // namespace store
+}  // namespace mybitcask
