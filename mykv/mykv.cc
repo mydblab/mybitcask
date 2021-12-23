c5c818fb81b50ebe58578ecc49552f9984cc95b3
0x5457 <0x5457@protonmail.com>
2021-12-24 01:50:15 +0800

    mykv

--- mykv/mykv.cc
+++ mykv/mykv.cc
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <regex>
+#include <string>
 
 #include "clipp.h"
+#include "mybitcask/mybitcask.h"
 #include "replxx.hxx"
 
 const std::string kPrompt = "\x1b[1;32mmykv\x1b[0m> ";
@@ -22,14 +24,25 @@ const std::regex kCommandRmRegex("rm\\s+(\\w+)\\s+");
 int main(int argc, char** argv) {
   std::string dbpath = "";
   bool check_crc = false;
-  auto cli =
-      (clipp::value("db path", dbpath), clipp::option("-c", "--checkcrc")
-                                            .set(check_crc)
-                                            .doc("Enable CRC verification"));
+  std::uint32_t dead_bytes_threshold = 128 * 1024 * 1024;
+
+  auto cli = (clipp::value("db path", dbpath),
+              clipp::option("-c", "--checksum")
+                  .set(check_crc)
+                  .doc("Enable CRC verification"),
+              clipp::option("-d", "--dead_bytes_threshold")
+                  .set(dead_bytes_threshold)
+                  .doc("maximum single log file size"));
   if (!clipp::parse(argc, argv, cli)) {
-    std::cout << clipp::make_man_page(cli, argv[0]);
+    std::cerr << clipp::make_man_page(cli, argv[0]);
     return 0;
   };
+  auto db = mybitcask::Open(dbpath, dead_bytes_threshold, check_crc);
+  if (!db.ok()) {
+    std::cerr << "Unable to start mybitask. error: " << db.status()
+              << std::endl;
+    return 0;
+  }
 
   // init the repl
   replxx::Replxx rx;
@@ -105,11 +118,25 @@ int main(int argc, char** argv) {
     }
     std::smatch sm;
     if (std::regex_match(input, sm, kCommandSetRegex)) {
-      std::cout << "set: " << sm[1] << sm[2] << std::endl;
+      auto status = (*db)->Insert(sm.str(1), sm.str(2));
+      if (!status.ok()) {
+        std::cerr << status << std::endl;
+      }
     } else if (std::regex_match(input, sm, kCommandGetRegex)) {
-      std::cout << "get: " << sm[1] << std::endl;
+      std::string v;
+      auto found = (*db)->Get(sm.str(1), &v, 2);
+      if (!found.ok()) {
+        std::cerr << found.status() << std::endl;
+      } else if (*found) {
+        std::cout << v << std::endl;
+      } else {
+        std::cout << "Key: '" << sm.str(1) << "' does not exist." << std::endl;
+      }
     } else if (std::regex_match(input, sm, kCommandRmRegex)) {
-      std::cout << "rm: " << sm[1] << std::endl;
+      auto status = (*db)->Delete(sm.str(1));
+      if (!status.ok()) {
+        std::cerr << status << std::endl;
+      }
     }
   }
 }
